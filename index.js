d37174b4ec727dcbda7e9eeb6b3159ddbe60961a
Raheem Moshood <raheemmoshood11@gmail.com>
2025-02-25 09:35:55 +0100

    Resolved merge conflicts

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const element = document.querySelector(".container")
+// const element = document.querySelector(".container")
+
+// function toggleTheme() {
+//     element.classList.toggle("light-mode");
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const elements = document.querySelectorAll(".fade-in");
+//     elements.forEach(el => el.classList.add("show"));
+// })
 
 function toggleTheme() {
+    const element = document.querySelector(".container");
     element.classList.toggle("light-mode");
+    localStorage.setItem("theme", element.classList.contains("light-mode") ? "light" : "dark");
 }
+
+window.onload = () => {
+    if (localStorage.getItem("theme") === "light") {
+        document.querySelector(".container").classList.add("light-mode");
+    }
+}
+
+
